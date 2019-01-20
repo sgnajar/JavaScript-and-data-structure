1cdd640a100d645b6b20304879e27c4da1b8b4de
Sasan Najar <sasangnajar@gmail.com>
2019-01-20 17:58:59 -0500

    create word-blanks.js

---
+++ word-blanks.js
@@ -0,0 +1,6 @@
+function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
+  var result = myNoun + " " + myAdjective + " " + myVerb + " " + myAdverb;
+  return result;
+}
+
+wordBlanks("cat", "little", "hit", "slowly");
