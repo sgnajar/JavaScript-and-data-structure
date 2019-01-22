238503ef44e46fdcdf88e7b25a17e45baa23ba35
Sasan Najar <sasangnajar@gmail.com>
2019-01-21 23:49:41 -0500

    initial commit

--- accessing-nested-obj.js
+++ accessing-nested-obj.js
@@ -1,3 +1,4 @@
+//Accessing Nested Arrays
 // Setup
 var myStorage = {
   "car": {
@@ -12,3 +13,26 @@ var myStorage = {
 };
 
 var gloveBoxContents = myStorage.car.inside["glove box"]; // maps
+
+//Accessing Nested Arrays
+// Setup
+var myPlants = [
+  {
+    type: "flowers",
+    list: [
+      "rose",
+      "tulip",
+      "dandelion"
+    ]
+  },
+  {
+    type: "trees",
+    list: [
+      "fir",
+      "pine",
+      "birch"
+    ]
+  }
+];
+
+var secondTree = myPlants[1].list[1]; // retrieve the second tree // pine
