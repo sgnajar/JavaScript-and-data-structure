f0b6cb134783fbebfcc96a5141dbbfec4d785dd1
Sasan Najar <sasangnajar@gmail.com>
2019-01-29 00:13:03 -0500

    Update basic-regex.js

--- regex/basic-regex.js
+++ regex/basic-regex.js
@@ -7,3 +7,32 @@ let result = myRegex.test(myString);
 let extractStr = "Extract the word 'coding' from this string.";
 let codingRegex = /coding/;
 let result = extractStr.match(codingRegex);
+
+// Regular Expressions: Find More Than the First Match
+let twinkleStar = "Twinkle, twinkle, little star";
+let starRegex = /twinkle/ig;
+let result = twinkleStar.match(starRegex);
+
+// Match Single Character with Multiple Possibilities
+let quoteSample = "Beware of bugs in the above code; I have only proved it correct, not tried it.";
+let vowelRegex = /[aeiou]/ig;
+let result = quoteSample.match(vowelRegex);
+
+// Match Letters of the Alphabet
+let quoteSample = "The quick brown fox jumps over the lazy dog.";
+let alphabetRegex = /[a-z]/ig;
+let result = quoteSample.match(alphabetRegex);
+
+// Match Characters that Occur Zero or More Times
+let chewieQuote = "Aaaaaaaaaaaaaaaarrrgh!";
+let chewieRegex = /Aa*/;
+let result = chewieQuote.match(chewieRegex);
+
+/*
+1) The only numbers in the username have to be at the end. There can be zero or more of them at the end.
+2) Username letters can be lowercase and uppercase.
+3) Usernames have to be at least two characters long. A two-letter username can only use alphabet letter characters.
+*/
+let username = "JackOfAllTrades";
+let userCheck = /\D{2,}/i;
+let result = userCheck.test(username);
