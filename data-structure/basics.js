43bc7688a4d92ad877fcf103defb7eec4446e610
Sasan Najar <sasangnajar@gmail.com>
2019-01-31 17:47:26 -0500

    Update basics.js

--- data-structure/basics.js
+++ data-structure/basics.js
@@ -1,9 +1,18 @@
 // return a new array that contains the elements 'warm' and 'sunny' using slice
-
 function forecast(arr) {
   arr = arr.slice(2,4);
   return arr;
 }
-
-// do not change code below this line
 console.log(forecast(['cold', 'rainy', 'warm', 'sunny', 'cool', 'thunderstorms']));
+
+// the function is supposed to return a new array made up of num copies of arr
+function copyMachine(arr, num) {
+  let newArr = [];
+  while (num >= 1) {
+    newArr = [[...arr], ...newArr];
+//    newArr.push([...arr]); works exactly as previous line!
+    num--;
+  }
+  return newArr;
+}
+console.log(([true, false, true], 2));
