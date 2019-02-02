cadbccac4992205ab6290380043a83c380a0fc47
Sasan Najar <sasangnajar@gmail.com>
2019-02-01 19:09:29 -0500

    Update basics.js

--- data-structure/basics.js
+++ data-structure/basics.js
@@ -16,3 +16,33 @@ function copyMachine(arr, num) {
   return newArr;
 }
 console.log(([true, false, true], 2));
+
+// Combine arrays with the spread operator
+function spreadOut() {
+  let fragment = ['to', 'code'];
+  let sentence = ['learning', ...fragment, 'is','fun'];
+  return sentence;
+}
+console.log(spreadOut());
+
+//  the function using indexOf() so that it returns true if the passed element exists on the array, and false if it does not.
+function quickCheck(arr, elem) {
+  if (arr.indexOf(elem) === -1){
+    return false;
+  } else {
+    return true;
+  }
+}
+console.log(quickCheck(['squash', 'onions', 'shallots'], 'mushrooms'));
+
+// Iterate Through All an Array's Items Using For Loops
+function filteredArray(arr, elem) {
+  let newArr = [];
+  for (var i=0; i < arr.length; i++){
+    if(arr[i].indexOf(elem) ===-1) {
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr;
+}
+console.log(filteredArray([[3, 2, 3], [1, 6, 3], [3, 13, 26], [19, 3, 9]], 3));
