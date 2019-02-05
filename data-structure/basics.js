e973d367fb33d7f21af36dbcfae1ff9d26cae637
Sasan Najar <sasangnajar@gmail.com>
2019-02-04 21:47:44 -0500

    Update basics.js

--- data-structure/basics.js
+++ data-structure/basics.js
@@ -59,3 +59,23 @@ foods['bananas'] = 13;
 foods['grapes'] = 35;
 foods['strawberries'] = 27;
 console.log(foods);
+
+// Create complex multi-dimensional arrays
+let myNestedArray = [
+  ['unshift', false, 1, 2, 3, 'complex', 'nested'],
+  ['loop', 'shift', 6, 7, 1000, 'method'],
+  [
+    ['deep']
+  ],
+  [
+    [
+      ['deeper']
+    ],
+    [
+      [
+        ['deepest']
+      ]
+    ],
+
+  ]
+];
